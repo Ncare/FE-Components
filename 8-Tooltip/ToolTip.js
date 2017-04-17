3eb8299fa7cb4e138d532701bed1df99753eaf1e
Ncare <xiongmingming@163.com>
2017-04-17 17:32:14 +0800

    tooltip update position

--- 8-Tooltip/ToolTip.js
+++ 8-Tooltip/ToolTip.js
@@ -1,7 +1,6 @@
+(function () {
 
-(function() {
-
-  var Tooltip = function(selector, opt) {
+  var Tooltip = function (selector, opt) {
 
     this.selector = selector;
     this.options = {
@@ -13,14 +12,14 @@
     this.els = new Array();
 
 
-    if(this.selector) {
+    if (this.selector) {
       this.els = document.querySelectorAll(this.selector);
-    }else {
+    } else {
       console.assert(this.selector === "", "no selector select!");
     }
 
-    if(opt && typeof opt === 'object') {
-      for(var key in opt) {
+    if (opt && typeof opt === 'object') {
+      for (var key in opt) {
         this.options[key] = opt[key];
       }
     }
@@ -39,19 +38,23 @@
     document.body.appendChild(this.tooltipDiv);
 
     this.tooltipDiv.style.opacity = '0';
-    
+
     // 每一个el添加mouseover事件
     var that = this;
-    for(var i=0; i<this.els.length; i++) {
-      (function(i){
-        that.els[i].addEventListener('mouseover', function(e){
+    for (var i = 0; i < this.els.length; i++) {
+      (function (i) {
+        that.els[i].addEventListener('mouseover', function (e) {
 
-          if(this.hasAttribute('data-title')) {
+          if (this.hasAttribute('data-title')) {
             that.options.tip = this.getAttribute('data-title');
-          }else if(this.hasAttribute('title')) {
+          } else if (this.hasAttribute('title')) {
             that.options.tip = this.getAttribute('title');
+          } else {
+            that.options.tip = 'That is a tip!';
           }
 
+          // 处理位置的问题
+          /*
           var content = document.querySelector('.t-content');
           content.innerHTML = that.options.tip;
 
@@ -64,30 +67,74 @@
           var a_height = arrow.offsetHeight;
 
           arrow.style.top = t_height;
-          arrow.style.left = t_width/2 - a_width/2;
-          
+          arrow.style.left = t_width / 2 - a_width / 2;
+
           var X = this.offsetLeft;
           var Y = this.offsetTop;
 
           var el_width = this.offsetWidth;
           var el_height = this.offsetHeight;
 
-          tooltip.style.left = X + el_width/2 - t_width/2;
+          tooltip.style.left = X + el_width / 2 - t_width / 2;
           tooltip.style.top = Y - t_height - a_height - 2;
 
           tooltip.style.opacity = '1'
 
-          setTimeout(function(e){
+          setTimeout(function (e) {
             tooltip.style.opacity = '0'
           }, 2000)
 
+          */
+
+          that.doWidthPosition(this);
         });
 
       })(i)
     }
   }
 
- 
+  Tooltip.prototype.doWidthPosition = function (obj) {
+
+    var content = document.querySelector('.t-content');
+    content.innerHTML = this.options.tip;
+
+    var tooltip = document.querySelector('.tooltip');
+    var t_width = tooltip.offsetWidth;
+    var t_height = tooltip.offsetHeight;
+
+    var arrow = document.querySelector('.t-arrow');
+    var a_width = arrow.offsetWidth;
+    var a_height = arrow.offsetHeight;
+
+    // 当前控件尺寸
+    var X = obj.offsetLeft;
+    var Y = obj.offsetTop;
+
+    var el_width = obj.offsetWidth;
+    var el_height = obj.offsetHeight;
+
+    if (this.options['position'] == "top") {
+      arrow.style.top = t_height;
+      arrow.style.left = t_width / 2 - a_width / 2;
+
+      tooltip.style.left = X + el_width / 2 - t_width / 2;
+      tooltip.style.top = Y - t_height - a_height - 2;
+    } else if (this.options['position']== "bottom") {
+      arrow.style.top = -a_height;
+      arrow.style.left = t_width / 2 - a_width / 2;
+      arrow.style.transform = 'rotate(180deg)';
+
+      tooltip.style.left = X + el_width / 2 - t_width / 2;
+      tooltip.style.top = Y + el_height + a_height;
+    }
+
+    tooltip.style.opacity = '1';
+
+    setTimeout(function (e) {
+      tooltip.style.opacity = '0'
+    }, 2000)
+
+  }
 
   window.Tooltip = Tooltip;
 })()
