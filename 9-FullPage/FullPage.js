8b4c95e2578605ac541216b36acac31ab6e3c040
Ncare <xiongmingming@163.com>
2017-04-20 17:42:15 +0800

    update

--- 9-FullPage/FullPage.js
+++ 9-FullPage/FullPage.js
@@ -1,7 +1,17 @@
 (function(){
 
-  var FullPage = function() {
+  var FullPage = function(opt) {
 
+    this.options = {
+      el: '',
+      duration: '1s'
+    }
+
+    if(opt && typeof opt == 'object') {
+      for(var key in opt) {
+        this.options[key] = opt[key];
+      }
+    }
   }
 
   window.FullPage = FullPage;
